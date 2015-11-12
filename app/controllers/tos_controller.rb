8fbc4e9fc40c21bbd7e04f9b0dce8049906f71ce
Justin Schoen <jaschoen@gmail.com>
2015-11-12 16:17:46 -0600

    add TOS and style them

---
+++ app/controllers/tos_controller.rb
@@ -0,0 +1,2 @@
+class TosController < ApplicationController
+end
---
+++ test/controllers/tos_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TosControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
